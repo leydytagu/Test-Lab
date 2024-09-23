a58f2b6a0bef0bf86bf9c7d0e1c3ff400f12abf3
leydytagu <leydy.tarazona@gmail.com>
2024-09-22 22:31:22 -0500

    Tarea 2 completa

--- AD-20-1 Assessment-Individual-1.0/ad-l11-1.0/js/user/user-controller.test.js
+++ AD-20-1 Assessment-Individual-1.0/ad-l11-1.0/js/user/user-controller.test.js
@@ -9,10 +9,53 @@ test('add user to userController', () => {
     expect(userController.getUsers()).toContain(user);
   });
 
+  test('does not add a user already in the list', () => {
+    let userController = new UserController();
+    let user = new User(1235, "Sara", "sara@generation.org");
+    userController.add(user);
+    expect(userController.getUsers()).toContain(user);
+  }); 
+
 test('remove user to userController', () => {    
     let user = new User(1234,"Santiago", "santiago@generation.org");
     userController.add(user);    
     userController.remove(user);
     expect(userController.users).not.toContain(user);
   });
+
+  test('remove user that is not in the list', () => {
+    const userController = new UserController();
+    const user = new User(1236, "Alex", "alex@generation.org");
+    expect(() => userController.remove(user)).not.toThrow();
+    expect(userController.getUsers()).not.toContain(user);
+  });
+
+  test('find user by email', () => {
+    const userController = new UserController();
+    const user = new User(1237, "Carolina", "carolina@generation.org");
+    userController.add(user);
+    expect(userController.findByEmail('carolina@generation.org')).toBe(user);
+  });
+
+  test('find user by email', () => {
+    const userController = new UserController();
+    const user = new User(1238, "Lucifer", "lucifer@generation.org");
+    userController.add(user);
+    expect(userController.findByEmail("lucifer@generation.org")).toBe(user);
+  });
+
+  test('find user by id', () => {
+    const userController = new UserController();
+    const user = new User(1239, "David", 'david@generation.org');
+    userController.add(user);
+    expect(userController.findById(1239)).toBe(user);
+  });
+
+  test('when id not found', () => {
+    const userController = new UserController();
+    expect(userController.findById(1240)).toBeUndefined();
+  });
+  
+  
+  
 
