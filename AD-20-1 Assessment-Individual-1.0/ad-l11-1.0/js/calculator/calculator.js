35a0496841e7f2236bfa8dd636035286085778c3
leydytagu <leydy.tarazona@gmail.com>
2024-09-22 21:59:04 -0500

    Tarea 1 completa

--- AD-20-1 Assessment-Individual-1.0/ad-l11-1.0/js/calculator/calculator.js
+++ AD-20-1 Assessment-Individual-1.0/ad-l11-1.0/js/calculator/calculator.js
@@ -14,4 +14,4 @@ function multiply(a, b){
 }
 
 // When you want to use the divide or multiple function, remember to add it to the export here
-module.exports = {add: add, subtract: subtract};
+module.exports = {add: add, subtract: subtract , divide:divide , multiply: multiply};
--- AD-20-1 Assessment-Individual-1.0/ad-l11-1.0/js/calculator/calculator.test.js
+++ AD-20-1 Assessment-Individual-1.0/ad-l11-1.0/js/calculator/calculator.test.js
@@ -15,3 +15,19 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+test('divide 10 / 2 to equal 5', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test('divide 60 / 12  to equal 5', () => {
+  expect(calculator.divide(60, 12)).toBe(5);
+});
+
+test('multiply 6 * 7 to equal 42', () => {
+  expect(calculator.multiply(6, 7)).toBe(42);
+});
+
+test('multiply -5 * 4 to equal -20', () => {
+  expect(calculator.multiply(-5, 4)).toBe(-20);
+});
